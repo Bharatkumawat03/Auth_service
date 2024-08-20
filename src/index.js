5c8f1bbc494281265c07dd793eb600e5cafc68d1
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-08-20 23:01:39 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+const db = require('./models/index');
+
 // const {User} = require('./models/index');
 // const bcrypt = require('bcrypt');
 
@@ -18,6 +20,12 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server started on port: ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
+
+
+
         // const incomingPassword = '123456';
         // const user = await User.findByPk(2);
         // const response = bcrypt.compareSync(incomingPassword, user.password);
